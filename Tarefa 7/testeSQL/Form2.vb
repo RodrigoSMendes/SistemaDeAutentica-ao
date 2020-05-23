004168d139c8730c623ad19cc035cd37215d6619
RodrigoSMendes <63783589+RodrigoSMendes@users.noreply.github.com>
2020-05-24 00:21:00 +0100

    Add files via upload

---
+++ Tarefa 7/testeSQL/Form2.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form2
+    Dim c As Integer = 0
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs)
+
+        Dim f As New Form3
+        f.Show()
+    End Sub
+
+    Private Sub waitaminute_Tick(sender As Object, e As EventArgs) Handles waitaminute.Tick
+        c += 1
+        If c = 1 Then
+            Me.Hide()
+            Form3.Show()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
---
+++ Tarefa 7/testeSQL/Form3.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form3))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtPasse = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Honeydew
+        Me.Label1.Location = New System.Drawing.Point(224, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Honeydew
+        Me.Label2.Location = New System.Drawing.Point(227, 195)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password:"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(253, 255)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(226, 59)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'txtLogin
+        '
+        Me.txtLogin.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtLogin.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtLogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLogin.ForeColor = System.Drawing.Color.DarkSlateGray
+        Me.txtLogin.Location = New System.Drawing.Point(330, 153)
+        Me.txtLogin.Multiline = True
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(168, 26)
+        Me.txtLogin.TabIndex = 2
+        '
+        'txtPasse
+        '
+        Me.txtPasse.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtPasse.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtPasse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPasse.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPasse.ForeColor = System.Drawing.Color.DarkSlateGray
+        Me.txtPasse.Location = New System.Drawing.Point(330, 191)
+        Me.txtPasse.Multiline = True
+        Me.txtPasse.Name = "txtPasse"
+        Me.txtPasse.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
+        Me.txtPasse.Size = New System.Drawing.Size(168, 26)
+        Me.txtPasse.TabIndex = 3
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(710, 396)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPasse)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.DarkSlateGray
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form3"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtLogin As TextBox
+    Friend WithEvents txtPasse As TextBox
+End Class
